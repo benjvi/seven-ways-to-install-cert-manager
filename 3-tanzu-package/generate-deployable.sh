694b48650956a6fff91903dcdaaedf49c6093c43
Ben Vickers <bvickers@vmware.com>
2021-05-21 17:58:07 +0200

    fix handling of ServiceAccount for package installation

--- 3-tanzu-package/generate-deployable.sh
+++ 3-tanzu-package/generate-deployable.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 klean-yaml() {
-  yq -y "del(.metadata.generation) | del(.metadata.managedFields) | del(.metadata.creationTimestamp) | del(.metadata.resourceVersion) | del(.metadata.uid) | del(.status)"
+  yq -y "del(.metadata.generation) | del(.metadata.managedFields) | del(.metadata.creationTimestamp) | del(.metadata.resourceVersion) | del(.metadata.uid) | del(.status) | del(.metadata.annotations[\"kubectl.kubernetes.io/last-applied-configuration\"])"
 }
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
@@ -13,6 +13,7 @@ TANZU_PKG="cert-manager.tce.vmware.com"
 tanzu package install "$TANZU_PKG" --config cert-manager.tce.vmware.com-values.yaml --namespace "$NAMESPACE"
 kubectl get secret "$TANZU_PKG-config" -n "$NAMESPACE" -o yaml | klean-yaml > "$SCRIPT_DIR/deploy/Secret.yml"
 kubectl get installedpackage "$TANZU_PKG" -n "$NAMESPACE" -o yaml | klean-yaml > "$SCRIPT_DIR/deploy/InstalledPackage.yml"
+kubectl get sa "$TANZU_PKG-extension-sa" -n "$NAMESPACE" -o yaml | klean-yaml | yq -y "del(.secrets)" > "$SCRIPT_DIR/deploy/ServiceAccount.yml"
 tanzu package delete "$TANZU_PKG" -n "$NAMESPACE"
 
 
